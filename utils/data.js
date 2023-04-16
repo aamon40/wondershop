9796c61ae41919e71d4a5e6c13a7423989e7d49e
Nyore <aamonthedon@gmail.com>
2023-04-16 15:33:59 +0100

    intergrate mongodb to store users

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,21 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "Maxwell",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "Max",
+      email: "user@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
       name: "Free Shirt",
